82f689f8d2866fb35f4b360b7a12bac9ff254f84
Luca <marino.luca07@gmail.com>
2019-02-11 17:55:55 +0100

    modifiche iniziale

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,32 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    Qui ci andrà tutto
+    <input type="text" v-model="input.company_id"> <br>{{input.company_id}} <br>
+    <input type="text" v-model="input.company_name"> <br>{{input.company_name}} <br>
+    <input type="text" v-model="input.company_email"> <br>{{input.company_email}} <br>
+    <input type="text" v-model="input.company_phone"> <br>{{input.company_phone}} <br>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'app',
-  components: {
-  }
+  name: "app",
+  data() {
+    return {
+      input: {
+        company_id: "",
+        company_name: "",
+        company_email: "",
+        company_phone: ""
+      }
+    };
+  },
+  components: {}
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
